e45200c170ae2584ea8e12e8c7385c9562a683f4
matiasbeckerle <matiasbeckerle@gmail.com>
2022-04-10 16:59:15 -0300

    Add supply mechanism

--- README.md
+++ README.md
@@ -15,3 +15,8 @@ The idea of this challenge is to create a dApp that has the ability to interact 
 - If connected to Kovan network, you can Deposit, Borrow, Repay and Redeem directly from: [https://app.compound.finance/](https://app.compound.finance/)
 - You can use this [Faucet](https://faucet.paradigm.xyz/) to get tokens
 - If you are going to use a subgraph, you can use [this one](https://thegraph.com/hosted-service/subgraph/juanigallo/cdai-kovan-subgraph) or create your own one
+
+## Kovan Testnet Network
+
+DAI contract: `0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa`
+cDAI contract: `0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD`
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { ethers } from 'ethers';
-import contractAbi from './contractAbi.json';
+import tokenContractAbi from './abi/tokenContract.json';
+import cTokenContractAbi from './abi/cTokenContract.json';
 import './App.css';
 
 // TODO: Move
@@ -10,35 +11,64 @@ declare global {
   }
 };
 
-const CONTRACT_ADDRESS = '0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD';
+// TODO: Move
+const TOKEN_CONTRACT_ADDRESS = '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa';
+const CTOKEN_CONTRACT_ADDRESS = '0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD';
 
 function App() {
   // TODO: Needed?
   const [provider, setProvider] = useState<ethers.providers.Web3Provider>();
-  const [contract, setContract] = useState<ethers.Contract>();
+  const [tokenContract, setTokenContract] = useState<ethers.Contract>();
+  const [cTokenContract, setCTokenContract] = useState<ethers.Contract>();
 
   const [accountAddress, setAccountAddress] = useState<string>();
-  const [balance, setBalance] = useState<number>();
+  const [tokenBalance, setTokenBalance] = useState<number>();
+  const [cTokenBalance, setCTokenBalance] = useState<number>();
 
   const accountChangeHandler = (accountAddress: string) => {
     setAccountAddress(accountAddress);
   };
 
-  const getBalance = async (accountAddress: string, contract: ethers.Contract) => {
-    let balance = await contract.balanceOf(accountAddress) / Math.pow(10, 2);
-    setBalance(balance);
+  // TODO: Reuse these two
+
+  const getTokenBalance = async (accountAddress: string, contract: ethers.Contract) => {
+    let balance = await contract.balanceOf(accountAddress) / 1e18;
+    setTokenBalance(balance);
+  }
+
+  const getCTokenBalance = async (accountAddress: string, contract: ethers.Contract) => {
+    let balance = await contract.balanceOf(accountAddress) / 1e8;
+    setCTokenBalance(balance);
+  }
+
+  async function supply() {
+    const tokenDecimals = 18;
+    const tokensToSupply = 1 * Math.pow(10, tokenDecimals);
+
+    let tx = await tokenContract?.approve(CTOKEN_CONTRACT_ADDRESS, tokensToSupply.toString());
+    await tx.wait(1);
+
+    tx = await cTokenContract?.mint(tokensToSupply.toString());
+    await tx.wait(1);
   }
 
   async function requestAccounts() {
     let temporalProvider = new ethers.providers.Web3Provider(window.ethereum);
     setProvider(temporalProvider);
-    let temporalContract = new ethers.Contract(CONTRACT_ADDRESS, contractAbi, temporalProvider);
-    setContract(temporalContract);
+
+    const signer = temporalProvider.getSigner();
+
+    let temporalTokenContract = new ethers.Contract(TOKEN_CONTRACT_ADDRESS, tokenContractAbi, signer);
+    setTokenContract(temporalTokenContract);
+
+    let temporalCTokenContract = new ethers.Contract(CTOKEN_CONTRACT_ADDRESS, cTokenContractAbi, signer);
+    setCTokenContract(temporalCTokenContract);
 
     try {
       let accounts = await temporalProvider.send('eth_requestAccounts', []);
       accountChangeHandler(accounts[0]);
-      await getBalance(accounts[0], temporalContract);
+      await getTokenBalance(accounts[0], temporalTokenContract);
+      await getCTokenBalance(accounts[0], temporalCTokenContract);
     } catch (error) {
       console.error(error);
       alert('Need to sign in to MetaMask');
@@ -55,7 +85,11 @@ function App() {
       {accountAddress ? (
         <div>
           <h2>{accountAddress}</h2>
-          <p>cDAI balance: {balance}</p>
+          <p>DAI balance: {tokenBalance}</p>
+          <p>cDAI balance: {cTokenBalance}</p>
+          <button onClick={supply}>
+            Supply 1 DAI
+          </button>
         </div>
       ) : (
         <button onClick={requestAccounts}>
